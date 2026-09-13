f2df4987da94cc14c74f34b442e2eba08eb4f7e8
MineTrainDevelopment <minetrain.dev@gmail.com>
2026-09-13 16:38:39 +0200

    Changed scaling to be based on screen.getHeight()

--- Pommes_KSP_Countdown/src/de/minetrain/contdown/frame/scaling/BaseScaling.java
+++ Pommes_KSP_Countdown/src/de/minetrain/contdown/frame/scaling/BaseScaling.java
@@ -4,11 +4,11 @@ import java.awt.GraphicsEnvironment;
 
 public class BaseScaling {
 
-	protected static final double REFERENCE_WIDTH = 1920;
+	protected static final double REFERENCE_HEIGHT = 1080;
 
 	protected static final double getScaling() {
-//		return (GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth() * 2) / REFERENCE_WIDTH;
-		return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth() / REFERENCE_WIDTH;
+//		return (GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getHeight() * 2) / REFERENCE_HEIGHT;
+		return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getHeight() / REFERENCE_HEIGHT;
 	}
 
 }
